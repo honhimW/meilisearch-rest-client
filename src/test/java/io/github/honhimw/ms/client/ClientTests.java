aa3b7205feda1b2a08d6c04fdb5a7966aaa79e6e
honhimW <honhimw@outlook.com>
2024-04-17 18:02:45 +0800

    Branch: v1.8

--- src/test/java/io/github/honhimw/ms/client/ClientTests.java
+++ src/test/java/io/github/honhimw/ms/client/ClientTests.java
@@ -43,7 +43,7 @@ public class ClientTests extends TestBase {
     @Test
     void version() {
         Version version = blockingClient.version();
-        assert StringUtils.startsWith(version.getPkgVersion(), "1.7");
+        assert StringUtils.startsWith(version.getPkgVersion(), "1.8");
     }
 
     @Order(1)
